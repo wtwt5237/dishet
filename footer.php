db77c9a301b282f963c80684acda75843d1d78a7
fangjiang <nickwfj123@outlook.com>
2025-05-28 11:37:09 -0500

    update license

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div class="footer">
     <p class="lf">2017 &copy; <a class="nounderline" href="https://qbrc.swmed.edu/">Quantitative Biomedical Research Center</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a class="nounderline" href="http://www.utsouthwestern.edu/">UT Southwestern Medical
-        Center</a></p>
+            Center</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a class="nounderline" href="license.txt">Terms and Conditions</a>
+    </p>
     <div class="clr"></div>
 </div>
